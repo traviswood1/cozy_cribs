e206c4e38c19fc705c27237bdc7f7b51a4802e80
Travis Wood <tww2013@gmail.com>
2024-09-18 19:58:41 -0500

    phase 3 complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,40 +1,10 @@
 // backend/routes/api/index.js
-const router = require('express').Router();
+const router = require("express").Router();
+const { restoreUser } = require("../../utils/auth.js");
 
-
-// backend/routes/api/index.js
-// ...
-
-
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
-
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-// GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-
-router.post('/test', function(req, res) {
-
-    res.json({ requestBody: req.body });
-  });
-  
-  // ...
-
 
 module.exports = router;

5362af5ad3430d5c87c8a7c429edcaa3780df665
Travis Wood <tww2013@gmail.com>
2024-09-23 19:30:25 -0500

    seeder fixing

--- backend/db/seeders/20240918210004-demo-user.js
+++ backend/db/seeders/20240918210004-demo-user.js
@@ -20,6 +20,8 @@ module.exports = {
         {
           email: 'demo@user.io',
           username: 'Demo-lition',
+          firstName: 'Demo',
+          lastName: 'User',
           hashedPassword: bcrypt.hashSync('password'),
           createdAt: new Date(),
           updatedAt: new Date()
@@ -27,6 +29,8 @@ module.exports = {
         {
           email: 'user1@user.io',
           username: 'FakeUser1',
+          firstName: 'Fake',
+          lastName: 'User1',
           hashedPassword: bcrypt.hashSync('password2'),
           createdAt: new Date(),
           updatedAt: new Date()
@@ -34,6 +38,8 @@ module.exports = {
         {
           email: 'user2@user.io',
           username: 'FakeUser2',
+          firstName: 'Fake',
+          lastName: 'User2',
           hashedPassword: bcrypt.hashSync('password3'),
           createdAt: new Date(),
           updatedAt: new Date()

815f0566a6d4f4c49def6a6832cf65b7ed07d764
Travis Wood <tww2013@gmail.com>
2024-09-23 19:55:54 -0500

    seeder fixing

--- backend/db/migrations/20240923212659-create-review.js
+++ backend/db/migrations/20240923212659-create-review.js
@@ -51,7 +51,7 @@ module.exports = {
         allowNull: false,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
-    });
+    }, options);
   },
 
   async down (queryInterface, Sequelize) {
--- backend/db/migrations/20240923213417-create-spotImage.js
+++ backend/db/migrations/20240923213417-create-spotImage.js
@@ -42,7 +42,7 @@ module.exports = {
         allowNull: false,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
-    });
+    }, options);
   },
 
   async down (queryInterface, Sequelize) {

0f502c8da181b9f008b739d8f8e2168da31df766
Travis Wood <tww2013@gmail.com>
2024-09-24 14:56:15 -0500

    seeders

--- backend/db/seeders/20240918210004-demo-user.js
+++ backend/db/seeders/20240918210004-demo-user.js
@@ -10,7 +10,8 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await User.bulkCreate([
+    options.tableName = 'Users';
+    await queryInterface.bulkInsert(options, [
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
@@ -32,14 +33,11 @@ module.exports = {
         lastName: 'Demo',
         firstName: 'User'
       }
-    ], { validate: true });
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
     options.tableName = 'Users';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
+    return queryInterface.bulkDelete(options, null, {});
   }
 };

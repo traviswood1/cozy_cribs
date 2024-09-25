a847733695405b0b0f5d1b54fccdf0767c36d021
Travis Wood <tww2013@gmail.com>
2024-09-25 11:26:58 -0500

    forgot to save

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -9,7 +9,6 @@ router.delete('/:imageId', requireAuth, async (req, res) => {
     const image = await SpotImage.findByPk(req.params.imageId);
 
     if (!image) {
-        console.log(`Image with id ${req.params.imageId} not found`);
         return res.status(404).json({ message: "Spot Image couldn't be found" });
     }
 
@@ -17,7 +16,6 @@ router.delete('/:imageId', requireAuth, async (req, res) => {
     const spot = await Spot.findByPk(image.spotId);
     
     if (!spot) {
-        console.log(`Spot with id ${image.spotId} not found`);
         return res.status(404).json({ message: "Spot couldn't be found" });
     }
 

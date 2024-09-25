314ee58504eb4f065f5bfb20524798c214d28320
JohnW44 <johnwintz@gmail.com>
2024-09-25 09:28:02 -0700

    Your commit message here

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,11 +4,7 @@ const { restoreUser } = require("../../utils/auth.js");
 const sessionRouter = require('./session.js');
 const usersRouter = require('./user.js');
 const spotRouter = require('./spot.js');
-<<<<<<< HEAD
 const spotImagesRouter = require('./spot-images.js');
-=======
-const reviewsRouter = require('./reviews.js');
->>>>>>> 625453c9412165d56281f95c79e11c2f9b705085
 
 // Connects restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
@@ -21,12 +17,8 @@ router.use('/users', usersRouter);
 
 router.use('/spots', spotRouter);
 
-<<<<<<< HEAD
 router.use('/spot-images', spotImagesRouter);
 
-=======
-router.use('/reviews', reviewsRouter);
->>>>>>> 625453c9412165d56281f95c79e11c2f9b705085
 router.post('/test', (req, res) => {
     console.log(`Received ${req.method} request to ${req.path}`);
   next();
